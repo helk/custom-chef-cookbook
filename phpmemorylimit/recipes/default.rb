3631b75c78dc516453c7b33b1a0b3b63e3259d17
Andreas Helgegren <andreas.helgegren@gmail.com>
2019-01-25 10:45:48 +0100

    uipdate cookbook

--- phpmemorylimit/recipes/default.rb
+++ phpmemorylimit/recipes/default.rb
@@ -1,6 +1,8 @@
+Chef::Log.info("Starting PHP Memory Limit update")
 bash "change_phpmemorylimit" do
   user "root"
   code <<-'EOH'
   sed -i 's/memory_limit = 128M/memory_limit = 256M/' /etc/php.ini
   EOH
 end
+Chef::Log.info("Ending PHP Memory Limit update")
