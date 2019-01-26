5685935628a448ec182f8f6930f3a7ddc1e41289
Andreas Helgegren <andreas.helgegren@gmail.com>
2019-01-26 14:05:30 +0100

    restart apache

--- phpmemorylimit/recipes/default.rb
+++ phpmemorylimit/recipes/default.rb
@@ -2,7 +2,11 @@ Chef::Log.info("Starting PHP Memory Limit update")
 bash "change_phpmemorylimit" do
   user "root"
   code <<-'EOH'
-  sed -i 's/memory_limit = 128M/memory_limit = 256M/' /etc/php.ini
+    sed -i 's/memory_limit = 128M/memory_limit = 256M/' /etc/php.ini
   EOH
+
+  action :nothing
+  notifies :restart, resources(:service => 'apache2')
+  timeout 70
 end
 Chef::Log.info("Ending PHP Memory Limit update")
